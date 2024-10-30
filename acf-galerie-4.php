83ef03d1aabc800ce2426703ccc2b519e3aa32ca
Navneil Naicker <hello@navz.me>
2024-10-30 20:36:34 +1200

    Tidy up the acf-galerie-4.php file.

--- acf-galerie-4.php
+++ acf-galerie-4.php
@@ -19,27 +19,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 define( 'ACFG4_VERSION', '1.2.0' );
-
 define( 'ACFG4_PLUGIN', __FILE__ );
-
 define( 'ACFG4_PLUGIN_BASENAME', plugin_basename( ACFG4_PLUGIN ) );
-
 define( 'ACFG4_PLUGIN_NAME', trim( dirname( ACFG4_PLUGIN_BASENAME ), '/' ) );
-
 define( 'ACFG4_PLUGIN_DIR', untrailingslashit( dirname( ACFG4_PLUGIN ) ) );
 
 //Load the text domain
 add_action('init', 'acfg4_textdomain');
 function acfg4_textdomain() {
-    load_plugin_textdomain( 'acf-galerie-4', false, basename( dirname( __FILE__ ) ) . '/lang' );
+	load_plugin_textdomain( 'acf-galerie-4', false, basename( dirname( __FILE__ ) ) . '/lang' );
 }
 
 //Registers the ACF field type.
 add_action( 'init', 'acfg4_init_register_type' );
 function acfg4_init_register_type() {
 	if ( ! function_exists( 'acf_register_field_type' ) ) return;
-
 	require_once __DIR__ . '/class-acfg4-register-field-type.php';
-
 	acf_register_field_type( 'acfg4_register_field_type' );
 }
