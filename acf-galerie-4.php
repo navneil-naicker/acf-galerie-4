567b7be0bbca144e249f42ee6ebd47dfb5b2bf58
Navneil Naicker <hello@navz.me>
2024-11-25 22:08:00 +1200

    Refactored the way we are getting attachments. Added code comments.

--- acf-galerie-4.php
+++ acf-galerie-4.php
@@ -24,13 +24,39 @@ define( 'ACFG4_PLUGIN_BASENAME', plugin_basename( ACFG4_PLUGIN ) );
 define( 'ACFG4_PLUGIN_NAME', trim( dirname( ACFG4_PLUGIN_BASENAME ), '/' ) );
 define( 'ACFG4_PLUGIN_DIR', untrailingslashit( dirname( ACFG4_PLUGIN ) ) );
 
-//Load the text domain
+/**
+ * Loads the text domain for translation in the plugin.
+ *
+ * Hooks into the `init` action to:
+ * - Load the plugin's text domain for translation, allowing the plugin to support
+ *   multiple languages.
+ * - Specifies the path to the translation files, located in the `lang` directory.
+ * - The `acf-galerie-4` text domain is used for translation strings within the plugin.
+ *
+ * This ensures that any text strings in the plugin can be translated according
+ * to the user's WordPress language settings.
+ *
+ * @return void
+ */
 add_action('init', 'acfg4_textdomain');
 function acfg4_textdomain() {
 	load_plugin_textdomain( 'acf-galerie-4', false, basename( dirname( __FILE__ ) ) . '/lang' );
 }
 
-//Registers the ACF field type.
+/**
+ * Registers the custom ACF field type during WordPress initialization.
+ *
+ * Hooks into the `init` action to:
+ * - Check if the `acf_register_field_type` function is available. 
+ *   This ensures compatibility with ACF Pro 5.0 or higher.
+ * - Require the file that defines the `acfg4_register_field_type` class.
+ * - Register the custom field type (`acfg4_register_field_type`) with ACF.
+ *
+ * This setup ensures that the `galerie-4` custom field type is properly
+ * initialized and available for use in ACF field groups.
+ *
+ * @return void
+ */
 add_action( 'init', 'acfg4_init_register_type' );
 function acfg4_init_register_type() {
 	if ( ! function_exists( 'acf_register_field_type' ) ) return;
@@ -38,87 +64,17 @@ function acfg4_init_register_type() {
 	acf_register_field_type( 'acfg4_register_field_type' );
 }
 
-function acfg4_get_gallery( $attachment_ids ){
-	$attachments = get_posts(
-		array(
-			'post_type' => 'attachment',
-			'post__in' => $attachment_ids,
-			'post_status' => 'inherit',
-			'orderby' => 'post__in',
-			'order' => 'ASC',
-			'numberposts' => -1
-		)
-	);
-
-	$graphql_format = [];
-
-	foreach( $attachments as $attachment ){
-		$l_graphql_format = [];
-		$l_graphql_format = [
-			'id' => $attachment->ID,
-			'postAuthor' => $attachment->post_author,
-			'postDate' => $attachment->post_date,
-			'postDateGmt' => $attachment->post_date_gmt,
-			'postContent' => $attachment->post_content,
-			'postTitle' => $attachment->post_title,
-			'postExcerpt' => $attachment->post_excerpt,
-			'postStatus' => $attachment->post_status,
-			'commentStatus' => $attachment->comment_status,
-			'pingStatus' => $attachment->ping_status,
-			'postName' => $attachment->post_name,
-			'toPing' => $attachment->to_ping,
-			'pinged' => $attachment->pinged,
-			'postModified' => $attachment->post_modified,
-			'postModifiedGmt' => $attachment->post_modified_gmt,
-			'postContentFiltered' => $attachment->post_content_filtered,
-			'postParent' => $attachment->post_parent,
-			'menuOrder' => $attachment->menu_order,
-			'postType' => $attachment->post_type,
-			'postMimeType' => $attachment->post_mime_type,
-			'commentCount' => $attachment->comment_count,
-			'filter' => $attachment->filter
-		];
-
-		if ( !preg_match('/image\/\w+/', $attachment->post_mime_type ) ) {
-			$file_url = wp_get_attachment_url( $attachment->ID );
-			array_push($l_graphql_format, ['file' => basename( $file_url )]);
-			array_push($l_graphql_format, ['mimeType' => $attachment->post_mime_type]);
-			array_push($l_graphql_format, ['fileSize' => $md['filesize'] ?? ""]);
-			array_push($l_graphql_format, ['fileUrl' => $file_url ?? ""]);
-		} else {
-			$md = wp_get_attachment_metadata( $attachment->ID );
-			$l_graphql_format['fullFile'] = $md['file'] ?? "";
-			$l_graphql_format['fullWidth'] = $md['width'] ?? "";
-			$l_graphql_format['fullHeight'] = $md['height'] ?? "";
-			$l_graphql_format['fullMimeType'] = $attachment->post_mime_type;
-			$l_graphql_format['fullFileSize'] = $md['filesize'];
-			$l_graphql_format['fullFileUrl'] = wp_get_attachment_image_src( $attachment->ID, 'full' )[0];
-			if( !empty( $md['sizes'] ) ){
-				foreach( $md['sizes'] as $key => $value ){
-					if( in_array($key, ['medium', 'large', 'thumbnail', 'medium_large']) ){
-						$file_url = wp_get_attachment_image_src( $attachment->ID, $key );
-						$key = str_replace( '-', '_', $key );
-						$key = str_replace( '_', '', $key );
-						$key = str_replace( 'filesize', 'fileSize', $key );
-						$value['fileUrl'] = !empty($file_url && $file_url[0]) ? $file_url[0] : "";
-	
-						$l_graphql_format[$key . 'FullFile'] = $value['fileUrl'];
-						$l_graphql_format[$key . 'fullWidth'] = $value['width'] ?? "";
-						$l_graphql_format[$key . 'fullHeight'] = $value['height'] ?? "";
-						$l_graphql_format[$key . 'fullMimeType'] = $value['mime-type'];
-						$l_graphql_format[$key . 'fullFileSize'] = $value['filesize'];
-						$l_graphql_format[$key . 'fullFileUrl'] = $value['fileUrl'];	
-					}
-				}
-			}
-		}
-		
-		array_push($graphql_format, $l_graphql_format);
-	}
-	
-	return $graphql_format;
-}
-
+/**
+ * Registers the custom ACF field type with WPGraphQL.
+ *
+ * Hooks into the `wpgraphql/acf/registry_init` action to initialize
+ * support for the custom `galerie-4` ACF field type in WPGraphQL.
+ *
+ * The function:
+ * - Requires the WPGraphQL integration class file.
+ * - Instantiates the `acfg4_wpgraphql` class to register and configure 
+ *   the field type within the WPGraphQL schema.
+ */
 add_action( 'wpgraphql/acf/registry_init', function() {
 	require_once __DIR__ . '/providers/class.wpgraphql.php';
 	new acfg4_wpgraphql();
