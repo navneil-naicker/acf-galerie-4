d159de1d9641ea86df26e0c508501171e65a64cb
Navneil Naicker <hello@navz.me>
2024-12-20 20:41:36 +1200

    Changes

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -2,23 +2,23 @@
   $(document).on("click", "a#acfg4-migrate", function (e) {
     e.preventDefault();
 
-    if ($("#acfg4-popup").length === 0) {
+    if ($("#acfg4-migrate-popup").length === 0) {
       const popupHtml = `
-          <div id="acfg4-popup-overlay"></div>
-          <div id="acfg4-popup">
+          <div id="acfg4-migrate-popup-overlay"></div>
+          <div id="acfg4-migrate-popup">
             <h2>ACF Galerie 4 — Migration</h2>
             <p>This tool will assist you to migrate from ACF Photo Gallery Field or ACF Gallery Pro to ACF Galerie 4.</p>
             <p>
-                <label>Choose plugin you want to migrate from?</label>
+                <label class="migrate-helper-text">Choose plugin you want to migrate from?</label>
                 <select>
                     <option>ACF Photo Gallery Field</option>
                     <option>ACF Gallery Pro</option>
                 </select>
-                <span>to</span>
-                <input value="ACF Galerie 4" readonly />
+                <span class="spacer-between-from-two">to</span>
+                <input type="text" value="ACF Galerie 4" readonly />
             </p>
-            <p>Important Note:</p>
-            <ul>
+            <p>Important Notes:</p>
+            <ul class="important-note-list">
                 <li>I have taken backup of my website.</li>
                 <li>I understand that not all the features will work as expected.</li>
                 <li>I understand that ACF Galerie 4 takes no responsbility for any damage done to my website.</li>
@@ -29,12 +29,12 @@
             </ul>
             <p>
                 <label>
-                    <input type="checkbox"/> I have read the important notice.
+                    <input type="checkbox"/> I have read the important notes.
                 </label>
             </p>
-            <div class="acfg4-action-buttons">
-                <button id="acfg4-close-popup">Close</button>
-                <button id="acfg4-start-migration">Start Migration</button>
+            <div class="action-buttons">
+                <button class="button" title="Cancel">Cancel</button>
+                <button class="button button-primary" title="Start Migration">Start Migration</button>
             </div>
           </div>
         `;
@@ -42,16 +42,18 @@
       $("body").append(popupHtml);
     }
 
-    $("#acfg4-popup, #acfg4-popup-overlay").fadeIn();
+    $("#acfg4-migrate-popup, #acfg4-migrate-popup-overlay").fadeIn();
   });
 
   $(document).on(
     "click",
-    "#acfg4-close-popup, #acfg4-popup-overlay",
+    "#acfg4-migrate-close-popup, #acfg4-migrate-popup-overlay",
     function () {
-      $("#acfg4-popup, #acfg4-popup-overlay").fadeOut(function () {
-        $(this).remove();
-      });
+      $("#acfg4-migrate-popup, #acfg4-migrate-popup-overlay").fadeOut(
+        function () {
+          $(this).remove();
+        }
+      );
     }
   );
 
